3ba1b21fdfbc00ff7506249124b22e54ad80457e
Hakimbek Bahramov <abduhakim.bahramv@gmail.com>
2022-06-30 13:51:56 +0500

    feat: migrate js to ts

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+
+module.exports = ({ development }) => ({
+    entry: './src/index.ts',
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        assetModuleFilename: "assets/[hash][ext]",
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets'},
+            ],
+        }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        ...esLintPlugin(development),
+    ]
+});
